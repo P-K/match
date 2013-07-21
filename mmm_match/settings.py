b446ad570156a9e737edae83a630b95c90e36fc8
P-K <preksha.kashyap@gmail.com>
2013-07-21 12:42:45 -0400

    removed extra join keyword in os join in settings file

--- mmm_match/settings.py
+++ mmm_match/settings.py
@@ -11,7 +11,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
@@ -175,7 +175,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join.join(PROJECT_PATH, 'static'),
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 
